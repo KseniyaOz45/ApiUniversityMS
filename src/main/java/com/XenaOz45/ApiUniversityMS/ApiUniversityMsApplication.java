a2bd7eb70e53648c524823c23a87459c91a2996c
Kseniya <xeniyaoz45@gmail.com>
2025-10-13 11:57:42 +0300

    Initial commit.

---
+++ src/main/java/com/XenaOz45/ApiUniversityMS/ApiUniversityMsApplication.java
@@ -0,0 +1,13 @@
+package com.XenaOz45.ApiUniversityMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiUniversityMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiUniversityMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/XenaOz45/ApiUniversityMS/ApiUniversityMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.XenaOz45.ApiUniversityMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiUniversityMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
